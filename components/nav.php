4f3383cd3a0de1fc80ca3e2cafa8837935e19a68
daviddiazm <davidcol123dm@gmail.com>
2024-03-07 12:07:56 -0500

    el nav ya funciona

--- components/nav.php
+++ components/nav.php
@@ -1,15 +1,11 @@
-<?php
-
-?>
-
-
 <nav class="header__nav">
   <ul class="header__nav-ul">
-    <li class="header__nav-item"><a href="" class="header__nav-link-logo-container"><img src="../assets/tendencias.png"
-          alt="" class="header__nav-link-logo-img"></a></li>
-    <li class="header__nav-item"><a href="" class="header__nav-link"></a>inicio</li>
-    <li class="header__nav-item"><a href="" class="header__nav-link"></a>Imuebles</li>
-    <li class="header__nav-item"><a href="" class="header__nav-link"></a>sobre nosotros</li>
-    <li class="header__nav-item"><a href="" class="header__nav-link"></a>contactanos</li>
+    <li class="header__nav-item"><a href="" class="header__nav-link-logo-container"><img src="../assets/tendencias.png" alt="" class="header__nav-link-logo-img"></a></li>
+    <li class="header__nav-item"><a href="../pages/home.php" class="header__nav-link">inicio</a></li>
+    <li class="header__nav-item"><a href="../pages/lista_inmuebles.php" class="header__nav-link">Imuebles</a></li>
+    <li class="header__nav-item"><a href="#" class="header__nav-link">sobre nosotros</a></li>
+    <li class="header__nav-item"><a href="#" class="header__nav-link">contactanos</a></li>
+    <li class="header__nav-item"><a href="../pages/subirInmbueble.php" class="header__nav-link">Trabaja con Nosotros</a></li>
+
   </ul>
 </nav>
