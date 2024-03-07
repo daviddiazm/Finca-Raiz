83bae0cfdd08867858847178cff4d392ce3db3b2
daviddiazm <davidcol123dm@gmail.com>
2024-03-07 12:07:08 -0500

    agregar el componente footer

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,22 @@
-<?php?>
 
-<footer>
+<footer class="footer">
+  <div class="footer__content">
+    <div class="footer__block">
+      <h4 class="footer__title">Contáctanos</h4>
+      <ul class="footer__list">
+        <li class="footer__item"><a href="#" class="footer__link">Teléfono: 123-456-789</a></li>
+        <li class="footer__item"><a href="#" class="footer__link">Email: info@bienesraices.com</a></li>
+        <li class="footer__item"><a href="#" class="footer__link">Dirección: Calle Principal, Ciudad</a></li>
+      </ul>
+    </div>
+    <div class="footer__block">
+      <h4 class="footer__title">Síguenos</h4>
+      <ul class="footer__list">
+        <li class="footer__item"><a href="#" class="footer__link footer__link--social">Facebook</a></li>
+        <li class="footer__item"><a href="#" class="footer__link footer__link--social">Twitter</a></li>
+        <li class="footer__item"><a href="#" class="footer__link footer__link--social">Instagram</a></li>
+      </ul>
+    </div>
+  </div>
+  <p class="footer__copyright">© 2024 Bienes Raíces. Todos los derechos reservados.</p>
 </footer>
