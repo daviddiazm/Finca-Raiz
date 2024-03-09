2156e2482d49b160594510762e9cf168517e110e
daviddiazm <davidcol123dm@gmail.com>
2024-03-09 01:05:45 -0500

    los rectangulos de redireccio ya van funcionando al igual que el video

--- pages/home.php
+++ pages/home.php
@@ -5,10 +5,36 @@
   <main class="main">
     <section class="main__canva">
       <div class="main__canva-cuadrilatero"></div>
-      <img src="../assets/screen_2x.webp" alt="" class="main__canva-img">
+      <article class="main__canva-text">
+        <h3 class="main__canva-text--h3">!Ven y revisa las mejores casas de tu ciudad!</h3>
+      </article>
+      <!-- <img src="../assets/screen_2x.webp" alt="" class="main__canva-img"> -->
+      <video src="../assets/videos/CGI - Animação Arquitetura - Casa Contemporânea (Blender).mp4" class="main__canva-img" controls></video>
     </section>
-    <section>
+    <section class="cards-container">
+      <?php include("../components/card.php") ?>
+      <?php include("../components/card.php") ?>
+      <?php include("../components/card.php") ?>
     </section>
+
+    <section class="redirectBoxes">
+      <articlle class="redirectBoxes__Box-big redirectBoxes__Box">
+        <h3 class="redirectBoxes__box-h3">Ven a ver tu nueva casa</h3>
+        <p class="redirectBoxes__box-p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam quas nisi odio eius asperiores corporis nemo ad doloremque. Facilis, ab!</p>
+        <img src="../assets/tendencias.png" class="redirectBoxes__box-img" alt="">
+      </articlle>
+      <articlle class="redirectBoxes__Box">
+        <h3 class="redirectBoxes__box-h3">Trabaja con nosotros</h3>
+        <p class="redirectBoxes__box-p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam quas nisi odio eius asperiores corporis nemo ad doloremque. Facilis, ab!</p>
+        <img src="../assets/tendencias.png" class="redirectBoxes__box-img" alt="">
+      </articlle>
+      <articlle class="redirectBoxes__Box">
+        <h3 class="redirectBoxes__box-h3">contactanos</h3>
+        <p class="redirectBoxes__box-p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam quas nisi odio eius asperiores corporis nemo ad doloremque. Facilis, ab!</p>
+        <img src="../assets/tendencias.png" class="redirectBoxes__box-img" alt="">
+      </articlle>
+    </section>
+
   </main>
   <?php include("../components/footer.php") ?>
 </body>
