b6adca9fbd01c8b29f523c29011e82e50be30354
daviddiazm <davidcol123dm@gmail.com>
2024-03-13 18:11:46 -0500

    el header ya redirecciona al login y register

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <header class="header">
   <?php include("../components/nav.php") ?>
   <section class="header__login-register-container">
-    <button class="header__login-register-btn">Registrate</button>
-    <button class="header__login-login-btn">Inicia sesion</button>
+    <button class="header__login-register-btn"><a href="../pages/loginSingUp.php">Inicia sesion</a></button>
+    <button class="header__login-login-btn"><a href="../pages/loginSingUp.php">Inicia sesion</a></button>
   </section>
 </header>
