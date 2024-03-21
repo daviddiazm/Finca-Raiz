09136674311fa0d3863ef9fa3fbfe65df4ab4051
daviddiazm <davidcol123dm@gmail.com>
2024-03-21 11:22:26 -0500

    aniadi 3 tipos de cards

---
+++ components/cardApartamento.php
@@ -0,0 +1,11 @@
+<section class="card">
+  <img src="../assets/screen_2x.webp" alt="" class="card__img">
+  <section class="card__text">
+    <p class="card__text-item--bolder">Apartamento</p>
+    <p class="card__text-item--bolder">2000000</p>
+    <p class="card__text-item">200mtr2</p>
+    <p class="card__text-item">Popayan-Cauca</p>
+    <p class="card__text-item--lighter">Cr48e #1-46</p>
+  </section>
+  <button class="card__buttom">Contactar</button>
+</section>
