21fed982915bdb1aca0c4f2b09516b34ca920ea0
daviddiazm <davidcol123dm@gmail.com>
2024-05-08 22:29:29 -0500

    cambios esteticos relacionados con el header

--- components/header.php
+++ components/header.php
@@ -1,7 +1,13 @@
 <header class="header">
   <?php include("../components/nav.php") ?>
   <section class="header__login-register-container">
-    <button class="header__login-register-btn"><a href="../pages/loginSingUp.php">Inicia sesion</a></button>
-    <button class="header__login-login-btn"><a href="../pages/loginSingUp.php">Inicia sesion</a></button>
+    <?php 
+    if(!$_SESSION){
+      echo '<button class="header__login-register-btn"><a href="../pages/loginSingUp.php">Inicia sesion - Registrate</a></button>';
+    }
+    if($_SESSION) {
+      echo '<button class="header__login-login-btn"><a href="../pages/closeSession.php">Cerrar Sesion</a></button>';
+    }
+    ?>
   </section>
 </header>
--- components/nav.php
+++ components/nav.php
@@ -1,11 +1,10 @@
 <nav class="header__nav">
   <ul class="header__nav-ul">
-    <li class="header__nav-item"><a href="" class="header__nav-link-logo-container"><img src="../assets/tendencias.png" alt="" class="header__nav-link-logo-img"></a></li>
-    <li class="header__nav-item"><a href="../pages/home.php" class="header__nav-link">inicio</a></li>
-    <li class="header__nav-item"><a href="../pages/lista_inmuebles.php" class="header__nav-link">Imuebles</a></li>
-    <li class="header__nav-item"><a href="#" class="header__nav-link">sobre nosotros</a></li>
-    <li class="header__nav-item"><a href="#" class="header__nav-link">contactanos</a></li>
-    <li class="header__nav-item"><a href="../pages/subirInmbueble.php" class="header__nav-link">Trabaja con Nosotros</a></li>
-
+    <a href="" class="header__nav-link-logo-container"><img src="../assets/tendencias.png" alt="" class="header__nav-link-logo-img"></a>
+    <a href="../pages/home.php" class="header__nav-link">inicio</a>
+    <a href="../pages/lista_inmuebles.php" class="header__nav-link">Imuebles</a>
+    <a href="#" class="header__nav-link">sobre nosotros</a>
+    <a href="#" class="header__nav-link">contactanos</a>
+    <a href="../pages/acpetarTerminosSubir.php" class="header__nav-link">Trabaja con Nosotros</a>
   </ul>
 </nav>
